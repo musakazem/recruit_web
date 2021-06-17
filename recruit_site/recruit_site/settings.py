78060f793b08d6aa68997aa536b3db715c450622
Musa Kazem <musakazem440@gmail.com>
2021-06-17 19:54:27 +0430

    heroku update

--- recruit_site/recruit_site/settings.py
+++ recruit_site/recruit_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,4 +140,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+#activate django heroku
+django_heroku.settings(locals())
